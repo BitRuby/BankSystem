470931899760c10fb6c7096c50034c21754afb3b
NorbertWychowski <norbertwychowski@gmail.com>
2018-07-16 16:32:41 +0200

    initial commit

---
+++ backend/src/main/java/com/onwelo/practice/bts/BtsApplication.java
@@ -0,0 +1,12 @@
+package com.onwelo.practice.bts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtsApplication.class, args);
+    }
+}
