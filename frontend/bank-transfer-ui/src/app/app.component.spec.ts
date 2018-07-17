fcdfc421ec5b4ab6d7c1665f56e19f9aa25b7bcf
firew <firewallosiem@gmail.com>
2018-07-17 10:30:34 +0200

    /feature/OP_16 - angular 6.0 project initialize

--- frontend/bank-transfer-ui/src/app/app.component.spec.ts
+++ frontend/bank-transfer-ui/src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to BankTransferSystem!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to bank-transfer-ui!');
   }));
 });
