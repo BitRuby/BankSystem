02261684e156b20aaa729b85cbc68d69a44b3472
Michał Zwierzyński <firewallosiem@gmail.com>
2018-07-27 09:06:54 +0200

    /feature/OP_18 - Code Review: Rearranged models

--- frontend/bank-transfer-ui/src/app/core/transfer/transfer.model.ts
+++ frontend/bank-transfer-ui/src/app/core/transfer/transfer.model.ts
@@ -1,14 +1,5 @@
-export class Transfer {
-  content: {
-    id: number;
-    title: string;
-    value: number;
-    accountId: string;
-    accountNo: string;
-    status: string;
-    transferType: string;
-    createTime: string;
-    bookingDate: string;
-    active: boolean;
-  };
+import {TransferContent} from "./transfer.content.model";
+
+export interface Transfer {
+  content: TransferContent;
 }
