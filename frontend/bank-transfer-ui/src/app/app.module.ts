555766d28bdf2d0d1ed9fc9dca57a9bf412e9589
Michał Zwierzyński <firewallosiem@gmail.com>
2018-07-30 11:15:48 +0200

    /feature/OP_18 - Added interceptor

--- frontend/bank-transfer-ui/src/app/app.module.ts
+++ frontend/bank-transfer-ui/src/app/app.module.ts
@@ -3,7 +3,9 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {NgModule} from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {AppInterceptor} from './app.interceptor';
+
 
 @NgModule({
   declarations: [
@@ -14,6 +16,9 @@ import {HttpClientModule} from '@angular/common/http';
     AppRoutingModule,
     NgbModule.forRoot(),
     HttpClientModule
+  ],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: AppInterceptor, multi: true}
   ],
   bootstrap: [AppComponent]
 })
